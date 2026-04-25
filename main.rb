0bc9c6071b329c2997c8770dea5592c259d931c0
songgeye <nov.twenty1120dibali@gmail.com>
2026-04-26 00:21:48 +0900

    例外処理の付いた電卓プログラムを作成

--- main.rb
+++ main.rb
@@ -1,21 +1,22 @@
 class Main
   def calculate(num1, num2, operator)
-      if num1 !~ /\A-?\d+\z/ || num2 !~ /\A-?\d+\z/
-        raise ArgumentError.new("num1、 num2 には整数を入力してください")
-      end
+    if num1 !~ /\A-?\d+\z/ || num2 !~ /\A-?\d+\z/
+      raise ArgumentError.new("num1、 num2 には整数を入力してください")
+    end
 
-      unless ['+', '-', '*', '/'].include?(operator)
-        raise StandardError.new("演算子には +、-、*、/ のいずれかを使用してください")
-      end
+    unless ['+', '-', '*', '/'].include?(operator)
+      raise StandardError.new("演算子には +、-、*、/ のいずれかを使用してください")
+    end
 
-    if operator == '+'
-      num1.to_i + num2.to_i
-    elsif operator == '-'
-      num1.to_i - num2.to_i
-    elsif operator == '*'
-      num1.to_i * num2.to_i
-    elsif operator == '/'
-      num1.to_i / num2.to_i
+    if operator == '/' && num2.to_i == 0
+      raise ZeroDivisionError, "ゼロによる割り算は許可されていません"
+    end
+
+    case operator
+    when '+' then num1.to_i + num2.to_i
+    when '-' then num1.to_i - num2.to_i
+    when '*' then num1.to_i * num2.to_i
+    when '/' then num1.to_i / num2.to_i
     end
   end
 end
@@ -33,11 +34,7 @@ begin
   main = Main.new
   result = main.calculate(num1, num2, operator)
   puts result
-rescue ArgumentError => e
-  puts e.message
-rescue ZeroDivisionError => _e
-  puts "ゼロによる割り算は許可されていません"
-rescue StandardError => e
+rescue ArgumentError, ZeroDivisionError, StandardError => e
   puts e.message
 end
 
